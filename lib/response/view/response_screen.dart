dbf17638afd773de93bc7cdda07dcfbe4cb66c0f
Roy <roymatero@gmail.com>
2021-10-29 22:50:11 +0300

    improve response ui

--- lib/response/view/response_screen.dart
+++ lib/response/view/response_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inter_intel_interview/app/utils/size_config.dart';
 import 'package:inter_intel_interview/response/bloc/todo_bloc.dart';
 import 'package:inter_intel_interview/response/model/todo.dart';
 import 'package:inter_intel_interview/response/repository/todo_repository.dart';
@@ -30,8 +32,13 @@ class _ResponseView extends StatelessWidget {
           );
         } else if (state is TodoLoadSuccess) {
           return ListView(
-            children: List.generate(state.todos.length,
-                (index) => TodoCard(todo: state.todos[index])),
+            children: List.generate(
+              state.todos.length,
+              (index) => TodoCard(
+                todo: state.todos[index],
+                index: index + 1,
+              ),
+            ),
           );
         } else {
           return const Center(child: Text('Failed to load data'));
@@ -42,18 +49,37 @@ class _ResponseView extends StatelessWidget {
 }
 
 class TodoCard extends StatelessWidget {
-  const TodoCard({Key? key, required this.todo}) : super(key: key);
+  const TodoCard({Key? key, required this.todo, required this.index})
+      : super(key: key);
 
   final Todo todo;
+  final int index;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(todo.title),
-      trailing: Checkbox(
-        value: todo.completed,
-        onChanged: (value) {},
-      ),
+    return Container(
+      margin: EdgeInsets.symmetric(
+          vertical: SizeConfig.unitHeight * 1,
+          horizontal: SizeConfig.unitWidth * 5),
+      padding: EdgeInsets.symmetric(
+          vertical: SizeConfig.unitHeight * 2,
+          horizontal: SizeConfig.unitWidth * 3),
+      decoration: BoxDecoration(
+          color: Theme.of(context).cardColor.withOpacity(.4),
+          borderRadius: BorderRadius.circular(20)),
+      child: ListTile(
+          title: Text(
+            todo.title,
+            style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                decoration: todo.completed ? TextDecoration.lineThrough : null),
+          ),
+          trailing: todo.completed
+              ? const Icon(
+                  CupertinoIcons.check_mark_circled,
+                  color: Colors.green,
+                  size: 30,
+                )
+              : null),
     );
   }
 }
